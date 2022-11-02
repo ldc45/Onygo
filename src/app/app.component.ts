5a6940b17b671d2a16f0ea2c14eeaf6af9d9961b
AGESILAS Ludovic <ldc45@outlook.fr>
2022-11-02 14:45:07 +0100

    test4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,28 +41,28 @@ this.monIntroduction = "Avec Onygo voyage à travers le monde !";
 ngOnInit():void{
 
 }
-//${this.inputDepart.value}
+
 public afficherResultat(){
 
-  return this.http.get(`https://restcountries.com/v2/capital/paris`)
+  return this.http.get(`https://restcountries.com/v2/capital/${this.inputDepart.value}`)
  .subscribe((data:any) => {console.log(this.codeDepart= data[0]['currencies'][0]['code'])
 
-//  return this.http.get(`https://restcountries.com/v2/capital/${this.inputDestination.value}`)
-//  .subscribe((data:any) => {this.codeDestination=data[0]['currencies'][0]['code'],data[0]['currencies'][0]['name']
+  return this.http.get(`https://restcountries.com/v2/capital/${this.inputDestination.value}`)
+  .subscribe((data:any) => {this.codeDestination=data[0]['currencies'][0]['code'],data[0]['currencies'][0]['name']
 
-//  return this.http.get(`https://v6.exchangerate-api.com/v6/${this.cleApi}/pair/${this.codeDepart}/${this.codeDestination}/${this.budget.value}`)
-//  .subscribe((data:any) => {this.montant = data.conversion_result.toFixed(2)
+  return this.http.get(`https://v6.exchangerate-api.com/v6/${this.cleApi}/pair/${this.codeDepart}/${this.codeDestination}/${this.budget.value}`)
+  .subscribe((data:any) => {this.montant = data.conversion_result.toFixed(2)
 
-// return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.inputDestination.value}&appid=${this.cleApi2}`)
-// .subscribe((data:any) => {this.temp = (data['main']['temp']-273.15).toFixed(0), this.codeImage = data['weather'][0]['icon'], this.description = data['weather'][0]['description']
+ return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.inputDestination.value}&appid=${this.cleApi2}`)
+ .subscribe((data:any) => {this.temp = (data['main']['temp']-273.15).toFixed(0), this.codeImage = data['weather'][0]['icon'], this.description = data['weather'][0]['description']
 
-// return this.http.get(`https://api.unsplash.com/search/photos?client_id=${this.cleApi3}&query=${this.inputDestination.value}-background`)
-// .subscribe((data:any) => {this.image=data['results'][this.rand]['urls']['regular']
+ return this.http.get(`https://api.unsplash.com/search/photos?client_id=${this.cleApi3}&query=${this.inputDestination.value}-background`)
+ .subscribe((data:any) => {this.image=data['results'][this.rand]['urls']['regular']
 
-// });
-// });
-// });
-// });
+ });
+ });
+ });
+ });
 });
 }
 }
