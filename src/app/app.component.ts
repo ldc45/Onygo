7efcba0912e73da29a235eab6dceac6c1d58d6fa
AGESILAS Ludovic <ldc45@outlook.fr>
2022-11-10 16:30:45 +0100

    e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,18 @@ export class AppComponent implements OnInit  {
 
 //Déclaration des variables
 
-public logo ="assets/images/Pink_Minimalist_Planet_Travel_Logo_Design_1.png"
-public testimage ="assets/images/maldives.jpg"
+public logo ="assets/images/logo.png"
 public monTitre : string;
 public monIntroduction : string;
 public montant : any;
 public codeDepart : any;
 public codeDestination : any;
 public codeImage : any;
-public temp : any;
+public temperature : any;
 public description : any;
 public image : any;
-public rand = Math.floor(Math.random() * (10 - 1) + 1);
+public alphaCode : any;
+public langage :any;
 
 private cleApi = "a92b35e3151ed3389e3aac02";
 private cleApi2 = "b9df352c5184221ff36bf83de98355c8";
@@ -33,12 +33,11 @@ private cleApi3 = "goLxS35I_CQHM3MjK7FPnznmQrKYGksKqEjSsu5UfrE";
 inputDepart = new FormControl('');
 inputDestination = new FormControl('');
 budget= new FormControl('');
-data: any;
+
 
 constructor(private http: HttpClient){ 
 this.monTitre = "Onygo";
 this.monIntroduction = "Avec Onygo voyage à travers le monde !";
-
 }
 
 ngOnInit():void{
@@ -48,16 +47,17 @@ ngOnInit():void{
 public afficherResultat(){
 
   return this.http.get(`https://restcountries.com/v2/capital/${this.inputDepart.value}`)
- .subscribe((data:any) => {console.log(this.codeDepart= data[0]['currencies'][0]['code'])
+ .subscribe((data:any) => {this.codeDepart= data[0]['currencies'][0]['code']
 
   return this.http.get(`https://restcountries.com/v2/capital/${this.inputDestination.value}`)
-  .subscribe((data:any) => {this.codeDestination=data[0]['currencies'][0]['code'],data[0]['currencies'][0]['name']
+  .subscribe((data:any) => {
+    this.codeDestination=data[0]['currencies'][0]['code'],data[0]['currencies'][0]['name'], this.alphaCode=data[0]['alpha2Code'],console.log(data[0]['languages'][0]['iso639_1'])
 
   return this.http.get(`https://v6.exchangerate-api.com/v6/${this.cleApi}/pair/${this.codeDepart}/${this.codeDestination}/${this.budget.value}`)
-  .subscribe((data:any) => {this.montant = data.conversion_result.toFixed(2)
+  .subscribe((data:any) => {this.montant = new Intl.NumberFormat('de-DE', { style: 'currency', currency: `${this.codeDestination}` }).format(data.conversion_result);
 
  return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.inputDestination.value}&appid=${this.cleApi2}`)
- .subscribe((data:any) => {this.temp = (data['main']['temp']-273.15).toFixed(0), this.codeImage = data['weather'][0]['icon'], this.description = data['weather'][0]['description']
+ .subscribe((data:any) => {this.temperature = (data['main']['temp']-273.15).toFixed(0), this.codeImage = data['weather'][0]['icon'], this.description = data['weather'][0]['description']
 
  return this.http.get(`https://api.unsplash.com/search/photos?client_id=${this.cleApi3}&query=${this.inputDestination.value}`)
  .subscribe((data:any) => {
