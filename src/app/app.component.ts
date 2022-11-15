521d0050d27a6c88849c60c00f891ffa1dca2a66
AGESILAS Ludovic <ldc45@outlook.fr>
2022-11-15 23:53:24 +0100

    boucle prevision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent implements OnInit  {
   title = 'Onygo';
 
 //Déclaration des variables
 
 public logo ="assets/images/logo.png"
+public testimage ="assets/images/maldives.jpg"
 public monTitre : string;
 public monIntroduction : string;
 public montant : any;
 public codeDepart : any;
 public codeDestination : any;
 public codeImage : any;
-public temperature : any;
+public temp : any;
+public humidity : any;
+public windSpeed : any;
+public cityName: any;
+public feels: any;
 public description : any;
 public image : any;
-public alphaCode : any;
-public langage :any;
+public date: any;
+public jour:any;
+public continent : any;
 
+
+
+public rand = Math.floor(Math.random() * (10 - 1) + 1);
 
 private cleApi = "a92b35e3151ed3389e3aac02";
 private cleApi2 = "b9df352c5184221ff36bf83de98355c8";
@@ -35,32 +44,68 @@ private cleApi3 = "goLxS35I_CQHM3MjK7FPnznmQrKYGksKqEjSsu5UfrE";
 inputDepart = new FormControl('');
 inputDestination = new FormControl('');
 budget= new FormControl('');
-
+data: any;
 
 constructor(private http: HttpClient){ 
 this.monTitre = "Onygo";
 this.monIntroduction = "Avec Onygo voyage à travers le monde !";
+
 }
 
-
 ngOnInit():void{
- }
+
+}
 
 public afficherResultat(){
 
   return this.http.get(`https://restcountries.com/v2/capital/${this.inputDepart.value}`)
- .subscribe((data:any) => {this.codeDepart= data[0]['currencies'][0]['code']
+ .subscribe((data:any) => {console.log(this.codeDepart= data[0]['currencies'][0]['code'])
 
   return this.http.get(`https://restcountries.com/v2/capital/${this.inputDestination.value}`)
-  .subscribe((data:any) => {
-    this.codeDestination=data[0]['currencies'][0]['code'],data[0]['currencies'][0]['name'], this.alphaCode=data[0]['alpha2Code'],console.log(data[0]['languages'][0]['iso639_1'])
+  .subscribe((data:any) => {this.codeDestination=data[0]['currencies'][0]['code'],data[0]['currencies'][0]['name']
+ 
+ this.continent = data[0].region
+ 
+ this.date =  new Date().toLocaleString('fr-FR', {dateStyle: 'full',timeStyle:'medium',timeZone: `${this.continent}/${this.inputDestination.value}`});
 
   return this.http.get(`https://v6.exchangerate-api.com/v6/${this.cleApi}/pair/${this.codeDepart}/${this.codeDestination}/${this.budget.value}`)
-  .subscribe((data:any) => {this.montant = new Intl.NumberFormat('de-DE', { style: 'currency', currency: `${this.codeDestination}` }).format(data.conversion_result);
+  .subscribe((data:any) => {this.montant = data.conversion_result.toFixed(2)
 
- return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.inputDestination.value}&appid=${this.cleApi2}`)
- .subscribe((data:any) => {this.temperature = (data['main']['temp']-273.15).toFixed(0), this.codeImage = data['weather'][0]['icon'], this.description = data['weather'][0]['description']
+ return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.inputDestination.value}&lang=fr&appid=${this.cleApi2}&units=metric`)
+ .subscribe((data:any) => { 
+ 
+ this.codeImage = data['weather'][0]['icon'], 
+ this.cityName= data.name;
+ this.temp  = `${data.main.temp.toFixed(0)}°C`;
+ this.description = data.weather[0].description;
+ this.feels = `${data.main.feels_like.toFixed(0)}°C`;
+ this.humidity = data.main.humidity;
+ this.windSpeed = data.wind.speed;
 
+
+ return this.http.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${this.inputDestination.value}&cnt=5&lang=fr&appid=${this.cleApi2}&units=metric`)
+ .subscribe((data:any) => { 
+  const jourSemaine = ['Dimanche','Lundi','Mardi','Mercredi','Jeudi', 'Vendredi', 'Samedi'] 
+  for (let i = 1, j = 0; i<5; i++, j++){
+    
+    const dt = data.list[i].dt
+    let im = document.createElement('img')
+    im.src =`http://openweathermap.org/img/wn/${data.list[i]['weather'][0]['icon']}.png`
+
+    let parJour = document.createElement('p')
+    parJour.innerText = jourSemaine[new Date(dt*1000).getDay()]
+
+    let tempPrev = document.createElement('p')
+    tempPrev.innerText = `${data.list[i].temp.day.toFixed(0)}°C`
+
+    const datemeteo = document.querySelectorAll('.btn-meteo')
+
+    datemeteo[j]?.appendChild(parJour)  
+    datemeteo[j]?.appendChild(tempPrev)
+    datemeteo[j]?.appendChild(im)
+ 
+
+  }
  return this.http.get(`https://api.unsplash.com/search/photos?client_id=${this.cleApi3}&query=${this.inputDestination.value}`)
  .subscribe((data:any) => {
   let section = document.createElement('section')
@@ -81,7 +126,7 @@ public afficherResultat(){
    } 
   
   //this.image=data['results'][this.rand]['urls']['regular']
-
+});
  });
  });
  });
@@ -90,9 +135,43 @@ public afficherResultat(){
 }
 }
 
-declare global {
-  interface Window {
-    myWidgetParam: any; // 
-  }}
 
-  window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 17,cityid: '2643743',appid: 'b9df352c5184221ff36bf83de98355c8',units: 'metric',containerid: 'openweathermap-widget-17',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode?.insertBefore(script, s);  })();
+
+
+
+
+
+
+
+// const ApiKey = '2e240a6b9222d721e04ea6d8b538464c';
+
+
+        
+// function recherche() {
+        
+
+
+//   let villeSaisie = document.querySelector("#ID_inputcity").value;
+  
+//   let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${villeSaisie}&appid=${ApiKey}&units=metric&lang=fr`;
+
+//    const date = new Date().toLocaleDateString(undefined, {dateStyle :'long'});
+
+
+//   //let urlIcon = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+
+// fetch(urlApi).then((response) =>
+// response.json().then((data) => {
+// console.log(data);
+// //document.querySelector('#ID_dateDuJour').innerHTML = date;
+
+
+
+// })
+// )
+// }
+
+
+
+// NE PAS TOUCHER LOL this.date =  new Date().toLocaleString('fr-FR', {timeZone: 'Europe/Paris'});
